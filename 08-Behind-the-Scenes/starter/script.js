17a7a4fe7f258af16dc29102fc3569598fef9db4
chzz <mail.chzzfly.com>
2023-01-02 14:52:42 +0800

    behind scenes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+
+  // 通过作用域链，它去找age和birthYear
+  function printAge() {
+    const outPut = `You are ${age}, born in ${birthYear}`;
+    console.log(outPut);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
