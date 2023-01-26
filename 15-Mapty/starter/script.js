ec532395e8a2deae19df6d011ef306127abc7d6a
chzzXx <mail@chzzfly.com>
2023-01-26 23:34:00 +0800

    create a workout object and render in map by leaflet

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -22,6 +22,7 @@ class App {
   // private filed，但是是所有通过class创建的对象共有的，理解private的含义：外部无法访问。
   #map;
   #mapEvent;
+  #workouts = [];
 
   // new 一个对象时，这个构造器会立即执行
   constructor() {
@@ -36,6 +37,7 @@ class App {
     inputType.addEventListener('change', this._toggleElevationfield);
   }
 
+  // 类原型对象prototype的方法，作为对象的原型proto
   _getPosition() {
     if (navigator.geolocation)
       // 这个方法需要两个回调函数，一个是成功的获取位置，进行操作；一个是获取失败的提示。
@@ -82,7 +84,57 @@ class App {
 
   // render workout form，渲染锻炼的输入表格-绑定事件：用户提交新的锻炼记录
   _newWorkout(e) {
+    // 这个函数太优雅了，使用左边的...扩展符，将所有参数放入一个数组中，然后使用数组的every方法判断每一个是否是数字
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+    // 检查是否全是正数
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
     e.preventDefault();
+
+    // 1.用户点击提交后，我们需要从表格里获取数据，
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
+
+    // 2.如果是running，创建running对象，
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      // 检查数据是否有效，如果通不过的话，直接return结束掉
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be positive numbers!');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+    // 2.如果是cycling，创建cycling对象；
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers!');
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+
+    // 3.将锻炼的情况，放在workout 数组里
+    // console.log(this);
+    this.#workouts.push(workout);
+    // console.log(workout, this.#workouts);
+    // 4.然后在地图上显示标记，
+    this._renderWorkoutMarker(workout);
+    // 5.在列表中显示锻炼情况
+    this._renderWorkoutList(workout);
+    // 6.隐藏表格输入框，情况input field
+
     // 清空输入框
     inputDistance.value =
       inputDuration.value =
@@ -90,8 +142,13 @@ class App {
       inputElevation.value =
         '';
     // console.log(mapEvent);
-    const { lat, lng } = this.#mapEvent.latlng;
-    L.marker([lat, lng])
+    // const { lat, lng } = this.#mapEvent.latlng;
+    // console.log(lat, lng);
+  }
+
+  // 渲染workout数据到地图上
+  _renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -99,12 +156,16 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent('cooooool!')
+      .setPopupContent(workout.id)
       .openPopup();
   }
+
+  // 渲染workout数据到列表里
+  // 需要一些DOM操作，将数据插入到HTML页面战中
+  renderWorkoutList(workout) {}
 }
 
 const app = new App();
@@ -123,7 +184,8 @@ class Workout {
   }
 }
 
-class Runing extends Workout {
+class Running extends Workout {
+  type = 'running';
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
@@ -138,6 +200,8 @@ class Runing extends Workout {
 }
 
 class Cycling extends Workout {
+  type = 'cycling';
+
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
@@ -151,6 +215,13 @@ class Cycling extends Workout {
   }
 }
 
-// const run1 = new Runing([39, 112], 5, 30, 178);
+// const run1 = new Running([39, 112], 5, 30, 178);
 // const cycling1 = new Cycling([39, 112], 27, 95, 523);
 // console.log(run1, cycling1);
+
+const runNew = new Running(
+  [],
+  inputDistance.value,
+  Number(inputDuration.value),
+  inputCadence.value
+);
