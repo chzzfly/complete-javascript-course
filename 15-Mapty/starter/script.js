b60ea89fdcb26557a9624f3925786a4b3a76ae5a
zhahuya <brblmdxj_1@163.com>
2023-01-27 19:22:08 +0800

    localstorage.setItem/getItem/removeItem

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -13,7 +13,80 @@ const inputGain = document.querySelector('.form__input--elevation');
 // 重构我们的代码
 // 建立两个大类：一个是App逻辑，一个是用户数据。
 // 所有对象公用的property，写在public field中，每个对象个性化的property，在new时需要传入参数的，写在constructor中
-// 建4个类，prettier一直报错？
+// 建4个类，prettier一直报错？因为把super写在constructor外面了
+
+// 数据类
+
+class Workout {
+  date = new Date();
+  // 如果有很多用户，有人在同时创建对象，那么依赖时间的ID就会有冲突
+  id = (Date.now() + '').slice(-10);
+  clicks = 0;
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+
+  _setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    }  ${this.date.getDate()}`;
+  }
+
+  click() {
+    this.clicks++;
+  }
+}
+
+class Running extends Workout {
+  type = 'running';
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.clacPace();
+    this._setDescription();
+  }
+
+  clacPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  type = 'cycling';
+
+  constructor(coords, distance, duration, gain) {
+    super(coords, distance, duration);
+    this.gain = gain;
+    this.clacSpeed();
+    this._setDescription();
+  }
+
+  clacSpeed() {
+    // km/hour
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, 112], 5, 30, 178);
+// const cycling1 = new Cycling([39, 112], 27, 95, 523);
+// console.log(run1, cycling1);
+
+const runNew = new Running(
+  [],
+  inputDistance.value,
+  Number(inputDuration.value),
+  inputCadence.value
+);
+
+// 逻辑类
 
 class App {
   // private filed，但是是所有通过class创建的对象共有的，理解private的含义：外部无法访问。
@@ -26,10 +99,16 @@ class App {
   // this，指向的是这个类创造的对象
   // console.log(this);
   constructor() {
+    // 获取用户的位置
     this._getPosition();
-    // 事件监听函数调用的回调函数“this._newWorkout"，它里面的this会指向form元素，而不是 App class 创建的对象，我们需要重新指向
+
+    // 从local storage 获取用户的数据
+    this._getLocalStorage();
+
+    // 用户提交事件。事件监听函数调用的回调函数“this._newWorkout"，它里面的this会指向form元素，而不是 App class 创建的对象，我们需要重新指向
     form.addEventListener('submit', this._newWorkout.bind(this));
-    // 切换跑步和骑车的输入框，这个回调函数里没有包含this，也就不需要处理
+
+    // 切换跑步和骑车的输入框。这个回调函数里没有包含this，也就不需要处理
     inputType.addEventListener('change', this._toggleGainfield);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
@@ -55,7 +134,7 @@ class App {
 
     const coords = [latitude, longitude];
     // _loadMap是被getCurrentPosition()方法调用的，普通的调用this不会有指向，因此需要绑定指向
-    console.log(this);
+    // console.log(this);
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel + 1);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -65,6 +144,8 @@ class App {
 
     // 在地图上点击，希望地图出现标记（标记不会关闭）；希望左侧列表出现输入框
     this.#map.on('click', this._showForm.bind(this));
+    // 等地图加载完成后，再渲染
+    this.#workouts.forEach(work => this._renderWorkoutMarker(work));
   }
 
   _showForm(mapE) {
@@ -138,7 +219,7 @@ class App {
       workout = new Cycling([lat, lng], distance, duration, gain);
     }
 
-    // 3.将锻炼的情况，放在 workout 数组里
+    // 3.将锻炼的情况，放在 workouts 数组里
     // console.log(this);
     this.#workouts.push(workout);
     // console.log(workout, this.#workouts);
@@ -148,6 +229,8 @@ class App {
     this._renderWorkoutList(workout);
     // 6.清空输入框中的数据，隐藏表格输入框
     this._hideForm();
+    // 7.把workouts数组存储到localStorage
+    this._setLocalStorage();
 
     // console.log(mapEvent);
     // const { lat, lng } = this.#mapEvent.latlng;
@@ -227,14 +310,14 @@ class App {
 
   _moveToPopup(e) {
     const workoutEl = e.target.closest('.workout');
-    console.log(workoutEl);
+    // console.log(workoutEl);
 
     if (!workoutEl) return;
 
     const workout = this.#workouts.find(
       work => work.id === workoutEl.dataset.id
     );
-    console.log(workout);
+    // console.log(workout);
 
     this.#map.setView(workout.coords, this.#mapZoomLevel + 1, {
       animate: true,
@@ -242,79 +325,30 @@ class App {
         duration: 1,
       },
     });
-    workout.click();
+
+    // workout.click();
+  }
+
+  _setLocalStorage() {
+    // key,value，不应该用localstorage存储大量数据
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+    // console.log(data);
+    if (!data) return;
+
+    this.#workouts = data;
+    this.#workouts.forEach(work => {
+      this._renderWorkoutList(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
 const app = new App();
-
-// 数据类
-
-class Workout {
-  date = new Date();
-  // 如果有很多用户，有人在同时创建对象，那么依赖时间的ID就会有冲突
-  id = (Date.now() + '').slice(-10);
-  clicks = 0;
-
-  constructor(coords, distance, duration) {
-    this.coords = coords; // [lat, lng]
-    this.distance = distance; // in km
-    this.duration = duration; // in min
-  }
-
-  _setDescription() {
-    // prettier-ignore
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
-      months[this.date.getMonth()]
-    }  ${this.date.getDate()}`;
-  }
-
-  click() {
-    this.clicks++;
-  }
-}
-
-class Running extends Workout {
-  type = 'running';
-  constructor(coords, distance, duration, cadence) {
-    super(coords, distance, duration);
-    this.cadence = cadence;
-    this.clacPace();
-    this._setDescription();
-  }
-
-  clacPace() {
-    // min/km
-    this.pace = this.duration / this.distance;
-    return this.pace;
-  }
-}
-
-class Cycling extends Workout {
-  type = 'cycling';
-
-  constructor(coords, distance, duration, gain) {
-    super(coords, distance, duration);
-    this.gain = gain;
-    this.clacSpeed();
-    this._setDescription();
-  }
-
-  clacSpeed() {
-    // km/hour
-    this.speed = this.distance / (this.duration / 60);
-    return this.speed;
-  }
-}
-
-// const run1 = new Running([39, 112], 5, 30, 178);
-// const cycling1 = new Cycling([39, 112], 27, 95, 523);
-// console.log(run1, cycling1);
-
-const runNew = new Running(
-  [],
-  inputDistance.value,
-  Number(inputDuration.value),
-  inputCadence.value
-);
