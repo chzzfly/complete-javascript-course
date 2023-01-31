7162255e0cd16e1a31760b93fc0f3ee6ccc4e9b2
chzzXx <mail@chzzfly.com>
2023-01-31 20:48:58 +0800

    async await in practice !

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -595,3 +595,130 @@ Promise.any([
   .then((res) => console.log(res))
   .catch((err) => console.error(err));
 */
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+
+// code challenge #2
+// const imgContainer = document.querySelector(".images");
+// const createImage = function (imgPath) {
+//   return new Promise(function (resolve, reject) {
+//     const img = document.createElement("img");
+//     img.src = imgPath; // 这本身就是个异步的操作
+//     // imgContainer.append(img); // 这里就可以直接加载，甚至不需要promise，resolve或reject
+
+//     img.addEventListener("load", function () {
+//       imgContainer.append(img);
+//       resolve(img);
+//     });
+
+//     img.addEventListener("error", function () {
+//       reject(new Error("Image not fond."));
+//     });
+//   });
+// };
+
+// let currentImg;
+
+// createImage("./img/img-1.jpg")
+//   .then((img) => {
+//     currentImg = img;
+//     console.log(` image 1 loaded`);
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//     return createImage("./img/img-2.jpg");
+//   })
+//   .then((img) => {
+//     currentImg = img;
+//     console.log(` image 2 loaded`);
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//     return createImage("./img/img-3.jpg");
+//   })
+//   .then((img) => {
+//     currentImg = img;
+//     console.log(` image 3 loaded`);
+//     return wait(2);
+//   })
+//   .catch((err) => console.error(err));
+
+// 利用async await 重写
+
+const imgContainer = document.querySelector(".images");
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", function () {
+      reject(new Error("Image not fond."));
+    });
+  });
+};
+
+// part1
+const loadNPause = async function () {
+  try {
+    // 返回值是一个promise函数，使用await取得settled值
+    let img = await createImage("img/img-1.jpg");
+    console.log(` image 1 loaded`);
+    await wait(2);
+    img.style.display = "none";
+    img = await createImage("img/img-2.jpg");
+    console.log(` image 2 loaded`);
+    await wait(2);
+    img.style.display = "none";
+    img = await createImage("img/img-3.jpg");
+    console.log(` image 3 loaded`);
+    await wait(2);
+    img.style.display = "none";
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// loadNPause();
+
+// part2
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async function (imgPath) {
+      return await createImage(imgPath);
+    });
+    console.log(imgs);
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach((img) => img.classList.add("parallel"));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadAll(["img/img-1.jpg", "img/img-2.jpg", "img/img-3.jpg"]);
