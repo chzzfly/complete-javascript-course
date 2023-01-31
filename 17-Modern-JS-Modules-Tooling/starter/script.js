dda1888dfca61963d88d761099a992c295c3db6c
chzzXx <mail@chzzfly.com>
2023-01-31 22:56:27 +0800

    export and import, named and default and top-level await

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,60 @@
+// Importing module
+
+// 1.导入部分值
+console.log('Importing module');
+// import { addToCart, totalPrice as price, tq } from './shopppingCart.js';
+// 无法访问module的值，因为那是private，不是global scoop的，必须要导出才能访问
+// console.log(shippingCost);
+// addToCart('辣条', 3);
+// console.log(price, tq);
+
+// 2. 导入所有export的
+// 将shoppingCart文件中所有export的东西都导入进来；并且给这些导入的值一个namespace.
+// 可以理解为模块是一个类，ShoppingCart 是根据类创建的一个对象，这个对象有方法也有property。也可以理解成一个public api，因为不导出的内容都是private。
+
+// import * as ShoppingCart from './shopppingCart.js';
+
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// 3. default exports and mix use，但我们从来不会混合使用它们！不要这么做！
+// import add, { addToCart, totalPrice as price, tq } from './shopppingCart.js';
+// console.log(price, tq);
+
+// 默认导出无须名字，通常一个模块只有一个，我们可以在导入时任意起名字，还不需要花括号。
+
+import add, { cart } from './shopppingCart.js';
+
+add('tomato', 33);
+add('bread', 3);
+add('apples', 333);
+
+// 为了示范 import的内容和export是联系的，live connetion
+console.log(cart);
+
+// await 可以在模块中 top-level 执行，但会阻塞模块的执行
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  // console.log(data);
+
+  return { titile: data.at(-1).titile, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+
+// async function 的返回值永远是 promise
+console.log(lastPost);
+
+// 为研究而使用then，永远不使用这玩意！
+// lastPost.then(last => console.log(last));
+
+// 使用 top-level await
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
---
+++ 17-Modern-JS-Modules-Tooling/starter/shopppingCart.js
@@ -0,0 +1,27 @@
+// Exporting module
+
+console.log('Exporting module');
+
+// 如果导入的模块中有top-level await，它会等待这个执行完才能完成模块的导入。
+
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
+
+const shippingCost = 10;
+export const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
