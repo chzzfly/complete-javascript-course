d4df1a75d9c3ae0e1603c6e8333208f2b773b2de
chzzXx <mail@chzzfly.com>
2023-02-02 21:46:50 +0800

    more clean code with functional paradigm

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,7 +1,10 @@
-// 'use strict'; 在module模块中隐含
+'use strict'; // 在module模块中隐含;
+
+// 概念：所有变量、数据的不变性，纯函数，更多使用三元运算符
 
 // 程序；花费清单，每人有金额限制，可以检查是否超出金额，可以检查多于某金额的支出。
-const budget = [
+// 应用函数式编程的不变性，冻结对象。事实上，冻结只能浅冻结，也就是冻结第一层。
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -10,61 +13,93 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
-};
+});
 
 // 和 || 一样，遇到ture直接返回；如果不是，后面的0对于？？来说也是true，只有undefined和null 才是false
 // 如果这个对象里有user则返回user的值，如果没有则返回0
-const getLimit = user => spendingLimits?.[user] ?? 0;
+// const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+const getLimit = (limits, user) => limits?.[user] ?? 0;
 
-const addExpense = function (value, description, user = 'jonas') {
-  // 设置参数的默认值
-  // if (!user) user = 'jonas';
-  user = user.toLowerCase();
+// 这个函数不是pure function，因为它在修改它之外的数据 budget。我们已经冻结了budget，在这里最好弄个数据的拷贝。
+// 现在它已经是一个pure function了。
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
 
-  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+  return value <= getLimit(spendingLimits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
+};
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
 
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
+// budget 已经被冻结了，但还能被修改第二层的数据，如下第一行不可修改，第二行可以修改。
+// budget[0] = 233
+// newBudget3[0] = 233;
+
+// 这个函数修改了已冻结的对象数据，不可取，不是pure function，改一下。
+// const checkExpenses = function (state, limits) {
+//   // 这个代码修改了传入的newBudget3数组，不好！
+//   // state.forEach(entry => {
+//   //   if (-entry.value > getLimit(limits, entry.user)) {
+//   //     entry.flag = 'limit';
+//   //   }
+//   // });
+//   return state.map(entry => {
+//     return -entry.value > getLimit(limits, entry.user)
+//       ? { ...entry, flag: 'limit' }
+//       : entry;
+//   });
+// };
+
+// 使用箭头函数重写上面的代码：有点太聪明了，不如上面的阅读性好。
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    -entry.value > getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry
+  );
+
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
+
+// 依然是在不停的操作budget数组，这完全不科学！
+const logBigExpenses = function (state, bigLimit) {
+  const BigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    // .map(entry => entry.description.slice(-2))
+    // .join('/');
+    .reduce((str, entry) => `${str} / ${entry.description.slice(-2)}`, '');
+
+  const BigExpenses1 = BigExpenses.slice(2);
+  console.log(BigExpenses1);
+
+  // let output = '';
+  // for (const entry of budget)
+  //   output +=
+  //     entry.value <= -bigLimit ? `${entry.description.slice(-2)} /` : '';
+
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
 };
 
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
+// console.log(budget);
 
-console.log(budget);
-
-const checkExpenses = function () {
-  // for (const entry of budget) {
-  //   // 和 || 一样，遇到ture直接返回；如果不是，后面的0对于？？来说也是true，只有undefined和null 才是false
-  //   // const limit = spendingLimits?.[entry.user] ?? 0;
-  //   if (entry.value < -getLimit(entry.user)) {
-  //     entry.flag = 'limit';
-  //   }
-  // }
-  budget.forEach(entry => {
-    if (-entry.value > getLimit(entry.user)) {
-      entry.flag = 'limit';
-    }
-  });
-};
-checkExpenses();
-
-const logBigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget)
-    output +=
-      entry.value <= -bigLimit ? `${entry.description.slice(-2)} /` : '';
-
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
-
-console.log(budget);
-
-logBigExpenses(100);
+logBigExpenses(finalBudget, 100);
